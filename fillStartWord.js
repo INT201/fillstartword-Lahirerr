76211d06b7caeb45f9054814e773126a21d9fa8c
magzap <tanachart.mk@mail.kmutt.ac.th>
2022-09-04 22:29:22 +0700

    first commit

--- fillStartWord.js
+++ fillStartWord.js
@@ -1,6 +1,16 @@
 const { template } = require('@babel/core')
 
+
 function fillStartWord(startWord, word) {
-  //code here
+    if(word === undefined || word === null){
+    return undefined
+    } else if(word.includes(startWord)){
+      return word
+    }
+    return startWord+word
 }
+console.log(fillStartWord("1-2565-","Hello World"))
+console.log(fillStartWord("first","firststep"))
+
+
 module.exports = fillStartWord
